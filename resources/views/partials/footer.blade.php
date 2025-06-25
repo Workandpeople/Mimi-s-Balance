cc0a44d28dde0cf95e369ab152b0109c481cfa9f
DeDinnich <lucas.dinnichert.pro@gmail.com>
2025-06-25 23:45:43 +0200

    finish

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="bg-light py-3 border-top mt-5">
     <div class="container text-center">
         <small>&copy; {{ date('Y') }} — Créé par 
-            <a href="https://lucas.dinnichert.fr" target="_blank" rel="noopener noreferrer">
+            <a href="https://lucas-dinnichert.fr" target="_blank" rel="noopener noreferrer">
                 DINNICHERT Lucas
             </a>
         </small>
